96c4a4c99a5b047460c5d5956b2efdba0b8a0f4c
Dima Snisarenko <snisarenkodima@gmail.com>
2020-10-24 14:47:31 +0200

    feat: toggle animation

--- src/components/component-preview/component-preview.tsx
+++ src/components/component-preview/component-preview.tsx
@@ -1,27 +1,28 @@
-import { Component, h, Element, State } from '@stencil/core';
+import { Component, h, Element, State, Host } from '@stencil/core';
 import prettier from 'prettier/standalone';
 import parserHtml from 'prettier/parser-html';
 import parserPostCss from 'prettier/parser-postcss';
 import hljs from 'highlight.js/lib/core';
 import xml from 'highlight.js/lib/languages/xml';
+import { trimFirstLine } from './trimFirstLine';
 hljs.registerLanguage('xml', xml);
 
 @Component({
   tag: 'component-preview',
   styleUrl: 'component-preview.scss',
-  shadow: true,
+  shadow: false,
 })
 export class ComponentPreview {
   @Element() el: HTMLElement;
 
-  @State() isCodeVisible = true;
+  @State() isCodeVisible = false;
 
   private innerHTML: string;
 
   componentWillLoad() {
     this.innerHTML = hljs.highlight(
       'xml',
-      prettier.format(this.el.innerHTML, {
+      prettier.format(trimFirstLine(this.el.innerHTML), {
         parser: 'html',
         plugins: [parserHtml, parserPostCss],
       })
@@ -30,17 +31,36 @@ export class ComponentPreview {
 
   render() {
     return (
-      <div>
-        <div class="preview">
+      <Host>
+        <div class="component-preview__preview">
           <slot />
         </div>
-        <button onClick={() => (this.isCodeVisible = !this.isCodeVisible)}>
-          Toggle Code
-        </button>
-        {this.isCodeVisible && (
-          <pre class="hljs" innerHTML={this.innerHTML}></pre>
-        )}
-      </div>
+        <div class="component-preview__code-previewer">
+          <a
+            role="button"
+            tabindex="0"
+            class={{
+              'component-preview__toggle': true,
+              'component-preview__toggle--active': this.isCodeVisible,
+            }}
+            onClick={() => (this.isCodeVisible = !this.isCodeVisible)}
+          >
+            <span class="component-preview__toggle-chevron">‣</span>
+            Toggle code
+          </a>
+          <div
+            class={{
+              'component-preview__code-container': true,
+              'component-preview__code-container--visible': this.isCodeVisible,
+            }}
+          >
+            <pre
+              class="component-preview__code hljs"
+              innerHTML={this.innerHTML}
+            ></pre>
+          </div>
+        </div>
+      </Host>
     );
   }
 }
