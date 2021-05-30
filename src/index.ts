1588d5274c59a3554fb5e35ca3d1fb76d6807bce
Dima Snisarenko <snisarenkodima@gmail.com>
2021-05-30 19:19:27 +0200

    chore: set up demo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-export { Components, JSX } from './components';
+import './components/say-something.ts';
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <style>
+    :host {
+      font-family: sans-serif;
+    }
+  </style>
+
+  <div>
+    <h1>Web Components with Webpack Starter Kit</h1>
+
+    Text: <input type="text" />
+
+    <say-something></say-something>
+    <say-something color="red"></say-something>
+  </div>
+`;
+
+class App extends HTMLElement {
+  private $allSaySomething;
+  private $input;
+
+  constructor() {
+    super();
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.$input = shadowRoot.querySelector('input');
+    this.$input.addEventListener('input', this._handleChange.bind(this));
+
+    this.$allSaySomething = shadowRoot.querySelectorAll('say-something');
+  }
+
+  _handleChange() {
+    this.$allSaySomething.forEach((element) => {
+      element.setAttribute('text', this.$input.value);
+    });
+  }
+}
+
+window.customElements.define('my-app', App);
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const commonConfig = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+};
+
+const demoConfig = {
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  output: {
+    path: path.resolve(__dirname, 'demo'),
+  },
+};
+
+const devConfig = {
+  mode: 'development',
+};
+
+const prodConfig = {};
+
+module.exports = (env) => {
+  switch (true) {
+    case env.development:
+      return merge(commonConfig, demoConfig, devConfig);
+    case env.demo:
+      return merge(commonConfig, demoConfig);
+    case env.production:
+      return merge(commonConfig, prodConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+};
